210f86097ed4719e31e90e1953006f1584e92a50
Daryll Santos <daryll.santos@gmail.com>
2014-04-02 03:32:09 +0800

    add validations for presence

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :owner, presence: true
+  validates :priority, presence: true
+  validates :deadline, presence: true
 end
