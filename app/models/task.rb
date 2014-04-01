b633e7d365a34d86554318740aa53fa3184c4d7b
Daryll Santos <daryll.santos@gmail.com>
2014-04-02 04:13:54 +0800

    spruce up design a bit

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
   validates :owner, presence: true
   validates :priority, presence: true
   validates :deadline, presence: true
