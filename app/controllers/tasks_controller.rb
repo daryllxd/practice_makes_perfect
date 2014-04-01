74f20b26d72b38ed0403598a35d1528d45d5a567
Daryll Santos <daryll.santos@gmail.com>
2014-04-02 03:36:42 +0800

    create task.create with strict parameters

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,17 @@ class TasksController < ApplicationController
   def index
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "Created!"
+    else
+      redirect_to tasks_path, notice: @task.errors
+    end
+  end
+
+  def task_params
+    params.required(:task).permit(:name, :email, :owner, :priority, :deadline, :private)
+  end
 end
